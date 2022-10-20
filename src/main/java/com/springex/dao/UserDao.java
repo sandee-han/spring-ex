630df2c2d1275b92f29052cfe673d7a5656e8c5c
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-10-20 13:16:36 +0900

    Spring review, deleteAll, getCount

---
+++ src/main/java/com/springex/dao/UserDao.java
@@ -0,0 +1,106 @@
+package com.springex.dao;
+
+import com.springex.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class UserDao {
+    private ConnectionMaker cm;
+
+    public UserDao(ConnectionMaker cm) {
+        this.cm = cm;
+    }
+
+    public UserDao() {
+        this.cm = new AwsConnectionMaker();
+    }
+
+    public void add(User user) {
+        Map<String, String> env = System.getenv();
+        try {
+            // DB접속 (ex sql workbeanch실행)
+            Connection c = cm.makeConnection();
+
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+            pstmt.setString(1, user.getId());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getPassword());
+
+            // Query문 실행
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            c.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+        Map<String, String> env = System.getenv();
+        Connection c;
+        try {
+            // DB접속 (ex sql workbeanch실행)
+            c = cm.makeConnection();
+
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+            pstmt.setString(1, id);
+
+            // Query문 실행
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            User user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
+
+            rs.close();
+            pstmt.close();
+            c.close();
+
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAll() throws  SQLException {
+        Connection c = cm.makeConnection();
+
+        PreparedStatement ps = c.prepareStatement("DELETE FROM users");
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = cm.makeConnection();
+
+        PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+//        userDao.add();
+        User user = userDao.findById("6");
+        System.out.println(user.getName());
+    }
+}
---
+++ src/main/java/com/springex/dao/UserDaoFactory.java
@@ -0,0 +1,13 @@
+package com.springex.dao;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class UserDaoFactory {
+
+    @Bean
+    public UserDao awsUserDao() {
+        return new UserDao(new AwsConnectionMaker());
+    }
+}
